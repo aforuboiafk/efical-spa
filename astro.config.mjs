d405c5ec65385fed7e10705fce0eca564a937cee
Yama <KuChannel@hotmail.com>
2025-08-24 17:13:38 -0400

    se agrega favicon y robot txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; 
+import tailwindcss from '@tailwindcss/vite';
 import icon  from 'astro-icon';
 import preact from '@astrojs/preact';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]  
   },
-  integrations: [
-    icon({
-      include: {
-        mdi: ['certificate-outline', 'tools', 'cart', 'warehouse', 'air-conditioner',"map-marker",
-          "phone",
-          "email",
-          "clock-outline"]
-      }
-    }),
-    preact(),
-  ],
+  site: 'https://efical-spa.cl',
+  integrations: [icon({
+    include: {
+      mdi: ['certificate-outline', 'tools', 'cart', 'warehouse', 'air-conditioner',"map-marker",
+        "phone",
+        "email",
+        "clock-outline"]
+    }
+  }), preact(), sitemap()],
 });
