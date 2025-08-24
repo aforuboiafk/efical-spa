fdd12d1d50090b01543d3432fc1bc0ce88283951
Yama <KuChannel@hotmail.com>
2025-08-23 22:04:50 -0400

    se actualiza el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -32,11 +32,11 @@ interface InfoCard {
 export class HomeComponent {
   infoCards: InfoCard[] = [
     {
-      title: 'Misión',
+      title: 'MISIÓN',
       content: 'Brindar soluciones integrales en mantenimiento y certificación de equipos térmicos industriales, garantizando la eficiencia y seguridad en cada servicio.'
     },
     {
-      title: 'Visión',
+      title: 'VISIÓN',
       content: 'Ser líderes en el sector de mantenimiento y certificación de equipos térmicos, reconocidos por nuestra excelencia técnica y compromiso con la calidad.'
     }
   ];
@@ -44,27 +44,27 @@ export class HomeComponent {
   valueCards: ValueCard[] = [
     {
       icon: 'verified',
-      title: 'Certificaciones',
+      title: 'CERTIFICACIONES',
       description: 'Certificación profesional de calderas, hornos, análisis de gases y calibración de quemadores industriales. Certificación de estanques de petróleo y sus componentes según DS10 2012 MINSAL, análisis de gases y calibración de quemadores.'
     },
     {
       icon: 'build',
-      title: 'Mantenciones',
+      title: 'MANUTENCIONES',
       description: 'Servicio técnico especializado en equipos térmicos industriales, redes de vapor, condensado y climatización (calderas de agua caliente y aceite térmico). Redes de vapor y condensado, refractarios y climatización( calderas de agua caliente y aceite térmico)'
     },
     {
       icon: 'shopping_cart',
-      title: 'Ventas',
+      title: 'VENTAS',
       description: 'Equipos y repuestos originales para sistemas térmicos (RIELLO, ECOFLAM, ANWO, IVAR, SIME, PONAST), manómetros, hidrómetros, electroválvulas.'
     },
     {
       icon: 'hardware',
-      title: 'Ferretería Termoindustrial',
+      title: 'FERRETERÍA TERMOINDUSTRIAL',
       description: 'Suministros especializados para la industria térmica. Norma ISO o DIN, globo, corte rápido, compuerta, fierro fundido, inoxidable o bronce.'
     },
     {
       icon: 'engineering',
-      title: 'Desarrollo y mantención de sistemas VRV',
+      title: 'DESARROLLO Y MANTENCIÓN DE SISTEMAS VRV',
       description: 'Desarrollo y mantención de sistemas VRV, aire acondicionado o Chiller.'
     }
   ];
